4ee3f0f2970d3047dfe046713343fa2c35cb9947
Ian <iangrinspan7@gmail.com>
2023-06-12 23:38:59 -0300

    borro archivos de output

--- verisol-test-main/output_echidna_states/OutputTemp_transitions.sol
+++ verisol-test-main/output_echidna_states/OutputTemp_transitions.sol
@@ -518,25 +518,25 @@ contract Crowdfunding {
     uint max_block;
     uint goal;
     uint blockNumber;
+    uint balance;
 
     mapping(address => uint) backers;
     address[] backersArray = new address[](0);
     address[] auxArray;
     uint countBackers = 0;
     bool funded = false;
-    uint balance = 0;
 
     // function a() hasInitialized public {
     //     assert(!(!(max_block > blockNumber) && !(max_block < blockNumber && goal <= balance) && !(blockNumber > max_block && !funded && goal > balance && backersArray.length != 0) && balance > 0));
     //     assert(!(!(max_block > blockNumber) && !(max_block < blockNumber && goal <= balance) && !(blockNumber > max_block && !funded && goal > balance && backersArray.length != 0) && balance > 0 )) ;
     // }
 
-    function my_constructor(address payable _owner, uint _max_block, uint _goal, uint _blockNumber) public {
+    function my_constructor(uint _max_block, uint _goal, uint _balance, uint _blockNumber) public payable{
 		require(!has_initialized);
-        owner = _owner; 
+        owner = payable(msg.sender); 
         max_block = _max_block;
         goal = _goal;
-        balance = 0;
+        balance = msg.value;
         blockNumber = _blockNumber;
 		has_initialized = true;
     }
