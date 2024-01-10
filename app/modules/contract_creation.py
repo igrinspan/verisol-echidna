72b59444c51b8b96bbec0c12873338e0844565ad
Ian <iangrinspan7@gmail.com>
2024-01-10 14:01:56 -0600

    refactor and add fix_for_version_5 function

--- app/modules/contract_creation.py
+++ app/modules/contract_creation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import re
 
 from modules.file_manager import create_file, write_file, write_file_from_string, create_directory_2
 from modules.contract_config import Mode
@@ -12,11 +13,11 @@ from modules.contract_config import Mode
 # A los ContractCreators les entra un objeto config_variables, del que sacan todo lo necesario:
 # states, preconditions, extraconditions, function_names, function_variables...
 
-# En la clase ContractCreator me quedaron varias funciones que en realidad son específicas de cada subclase.
+# En la clase ContractCreator me quedaron varias funciones que en realidad son específicas de cada subclase (revisar).
 # Por ejemplo, change_for_constructor_fuzzing es sólo del EchidnaContractCreator y get_valid_preconditions_output del VerisolContractCreator.
 # Las únicas 2 funciones de las que se comparte la signatura pero no la implementación son create_init_contract y create_multiple_transitions_contracts.
 
-# Al final del módulo me quedaron algunas funciones sueltas que son usadas por funciones de los ContractCreators. Podrían ser agregadas a la superclase.
+# Al final del módulo me quedaron algunas funciones sueltas que son usadas por funciones de los ContractCreators.
 
 
 QUERIES_COUNT_THRESHOLD = 250
@@ -83,71 +84,21 @@ class ContractCreator:
                 break
         return target_constructor_start_line, target_constructor_end_line
 
-    def create_modifier(self, name, require_clauses):
-        res = f"\tmodifier {name} {{\n"
-        for require in require_clauses:
-            res += f"\t\trequire({require});\n"
-        res += "\t\t_;\n"
-        res += "\t}\n\n"
-        return res
-
-    def has_initialized_code(self):
-        has_initialized_modifier = self.create_modifier("hasInitialized", ["has_initialized"])
-        has_not_initialized_modifier = self.create_modifier("hasNotInitialized", ["!has_initialized"])
-        has_initialized_declaration = "\tbool has_initialized = false;\n\n"
-        return has_initialized_modifier + has_not_initialized_modifier + has_initialized_declaration
-
-    def change_for_constructor_fuzzing(self, contract_filename):
-        has_initialized_initial_code = self.has_initialized_code()
-        write_file(contract_filename, has_initialized_initial_code, self.target_contract)
-
-        new_lines = self.add_modifier_to_contract_functions(contract_filename, "hasInitialized")
-
-        change_status = "\t\t\t\thas_initialized = true;\n"
-        start, end = self.get_constructor_start_and_end_lines(new_lines)
-        new_lines = new_lines[: end - 1] + [change_status] + new_lines[end - 1 :]
-        new_lines[start] = (
-            new_lines[start].replace("constructor", "function my_constructor").replace(")", ") hasNotInitialized", 1)
-        )
-
-        write_file_from_string(contract_filename, new_lines)
-
-    def add_modifier_to_contract_functions(self, contract_filename, modifier_name):
-        lines = open(contract_filename, "r").readlines()
-        new_lines = []
-        in_contract = False
-        for line in lines:
-            if line.strip().startswith("contract " + self.target_contract) and not is_a_comment(line):
-                in_contract = True
-            if in_contract and line.strip().startswith("function") and not is_a_comment(line):
-                new_lines += [line.replace(")", f") {modifier_name}", 1)]  # el 1 indica la cantidad de reemplazos.
-            else:
-                new_lines += [line]
-        return new_lines
-
-    def get_valid_preconditions_output(self, preconditions, extra_conditions, function_variables):
-        temp_output = ""
-        temp_function_names = []
-        for id_precondition_require, precondition_require in enumerate(preconditions):
-            function_name = get_temp_function_name(id_precondition_require, "0", "0")
-            temp_function_names.append(function_name)
-            temp_function = functionOutput(function_name, function_variables) + "\n"
-            temp_function += output_valid_state(precondition_require, extra_conditions[id_precondition_require])
-            temp_output += temp_function + "\t}\n"
-        return temp_output, temp_function_names
-
-    def get_valid_transitions_output(self, preconditions, states, extra_conditions, functions, config_variables):
+    def get_valid_transitions_output(self, preconditions_thread, preconditions, states_thread, extra_conditions_thread, extra_conditions, functions, config_variables):
         # temp_output = ""
         all_queries_names = []
         all_queries = []
-        for index_precondition_require, precondition_require in enumerate(preconditions):
+        for index_precondition_require, precondition_require in enumerate(preconditions_thread):
+            if isinstance(preconditions, np.ndarray):
+                preconditions = preconditions.tolist()
+            real_index_precondition_require = preconditions.index(precondition_require)
             for index_precondition_assert, precondition_assert in enumerate(preconditions):
                 for index_function, function in enumerate(functions):
-                    extra_condition_pre = extra_conditions[index_precondition_require]
+                    extra_condition_pre = extra_conditions_thread[index_precondition_require]
                     extra_condition_post = extra_conditions[index_precondition_assert]
-                    if ((index_function + 1) in states[index_precondition_require] and config_variables.mode == Mode.epa) or config_variables.mode == Mode.states:
+                    if ((index_function + 1) in states_thread[index_precondition_require] and config_variables.mode == Mode.epa) or config_variables.mode == Mode.states:
                         # Abstraer esto de acá abajo en una función aparte
-                        function_name = get_temp_function_name(index_precondition_require, index_precondition_assert, index_function)
+                        function_name = get_temp_function_name(real_index_precondition_require, index_precondition_assert, index_function) # Uso el real_index_precondition_require para el nombre de la query.
                         temp_function = functionOutput(function_name, config_variables.functionVariables) + "\n"
                         temp_function += output_transitions_function(
                             precondition_require,
@@ -162,7 +113,7 @@ class ContractCreator:
                         # temp_output += temp_function
                         all_queries_names.append(function_name)
                         all_queries.append(temp_function)
-
+        
         return all_queries, all_queries_names
 
     def get_init_output(self, config_variables):
@@ -195,11 +146,14 @@ class EchidnaContractCreator(ContractCreator):
         return filename_temp, all_queries_names
 
     def create_multiple_transitions_contracts(self, count):
-        queries, function_names = self.get_valid_transitions_output(self.config_variables.preconditions,self.config_variables.states,self.config_variables.extraConditions,self.config_variables.functions,self.config_variables,)
+        pre = self.config_variables.preconditions
+        extra = self.config_variables.extraConditions
+        queries, function_names = self.get_valid_transitions_output(pre, pre, self.config_variables.states, extra, extra, self.config_variables.functions, self.config_variables)
 
         # De function_names podemos sacar la cantidad de queries que se crearon.
         queries_count = len(function_names)
         contracts = []
+        print(f"queries_count={queries_count}")
         if queries_count > QUERIES_COUNT_THRESHOLD:
             splits = count
             queries_splitted = np.array_split(queries, splits)  # Crea un arreglo de arreglos de queries.
@@ -225,6 +179,64 @@ class EchidnaContractCreator(ContractCreator):
 
         return contracts, None
 
+    def change_for_constructor_fuzzing(self, contract_filename):
+        has_initialized_initial_code = self.has_initialized_code()
+        write_file(contract_filename, has_initialized_initial_code, self.target_contract)
+
+        new_lines = self.add_modifier_to_contract_functions(contract_filename, "hasInitialized")
+        change_status = "\thas_initialized = true;\n"
+        start, end = self.get_constructor_start_and_end_lines(new_lines)
+
+        constructor_lines = new_lines[start:end]
+        for index, line in enumerate(constructor_lines):
+            if ")" in line:
+                if "public" in line:
+                    constructor_lines[index] = line.replace(")", ") hasNotInitialized ", 1)
+                else:
+                    constructor_lines[index] = line.replace(")", ") hasInitialized public", 1)
+                break
+
+        constructor_lines[0] = constructor_lines[0].replace("constructor", "function my_constructor")
+        constructor_lines[-1] = constructor_lines[-1].replace("}", f"{change_status} {constructor_lines[-1]}")
+
+        new_lines = new_lines[:start] + constructor_lines + new_lines[end:]
+        write_file_from_string(contract_filename, new_lines)
+
+    def add_modifier_to_contract_functions(self, contract_filename, modifier_name):
+        lines = open(contract_filename, "r", encoding='utf-8').readlines()
+        new_lines = []
+        in_contract = False
+        in_function_declaration = False
+
+        for line in lines:
+            if is_contract_declaration(line, self.target_contract):
+                in_contract = True
+            elif in_contract and is_function_declaration(line):
+                in_function_declaration = True
+
+            if in_function_declaration and ")" in line:
+                line = line.replace(")", f") {modifier_name}", 1)
+                in_function_declaration = False
+
+            line = line.replace("pure", "view") # porque el modifier accede a una variable de estado, deja de ser pure.
+            new_lines.append(line)
+
+        return new_lines
+
+    def has_initialized_code(self):
+        has_initialized_modifier = self.create_modifier("hasInitialized", ["has_initialized"])
+        has_not_initialized_modifier = self.create_modifier("hasNotInitialized", ["!has_initialized"])
+        has_initialized_declaration = "\tbool has_initialized = false;\n\n"
+        return has_initialized_modifier + has_not_initialized_modifier + has_initialized_declaration
+
+    def create_modifier(self, name, require_clauses):
+        res = f"\tmodifier {name} {{\n"
+        for require in require_clauses:
+            res += f"\t\trequire({require});\n"
+        res += "\t\t_;\n"
+        res += "\t}\n\n"
+        return res
+
 
 class VerisolContractCreator(ContractCreator):
     def __init__(self, config_variables):
@@ -239,6 +251,7 @@ class VerisolContractCreator(ContractCreator):
         body, all_queries_names = self.get_valid_preconditions_output(preconditions, extraconditions, self.config_variables.functionVariables)
         body = self.clean_true_requires(body)
         write_file(filename_temp, body, self.target_contract)
+        fix_for_version_5(filename_temp)
         filename_temp = os.path.realpath(filename_temp)
         return filename_temp, all_queries_names
 
@@ -247,6 +260,7 @@ class VerisolContractCreator(ContractCreator):
         body, all_queries_names = self.get_init_output(self.config_variables)
         body = self.clean_true_requires(body)
         write_file(filename_temp, body, self.target_contract)
+        fix_for_version_5(filename_temp)
         self.transform_contract_for_init(filename_temp)
         filename_temp = os.path.realpath(filename_temp)
         return filename_temp, all_queries_names
@@ -255,7 +269,9 @@ class VerisolContractCreator(ContractCreator):
         contracts = []
         # queries_names_per_contract = []
 
-        queries, queries_names = self.get_valid_transitions_output(self.config_variables.preconditions,self.config_variables.states,self.config_variables.extraConditions,self.config_variables.functions,self.config_variables,)
+        pre = self.config_variables.preconditions
+        extra = self.config_variables.extraConditions
+        queries, queries_names = self.get_valid_transitions_output(pre, pre, self.config_variables.states, extra, extra, self.config_variables.functions,self.config_variables)
 
         queries_splitted = np.array_split(queries, count)  # Crea un arreglo de arreglos de queries.
         names_splitted = np.array_split(queries_names, count)
@@ -267,8 +283,8 @@ class VerisolContractCreator(ContractCreator):
             body = ''.join(queries_list)
             body = self.clean_true_requires(body)
             new_dir = create_directory_2(self.config_variables.dir, f"thread_{idx}")
-            # create_file hace un archivo nuevo copiando los contenidos de EPXCrowdsale.sol
-            filename_temp = create_file(f"transitions_{idx}", new_dir, self.config_variables.fileName, self.target_contract)
+            # create_file hace un archivo nuevo copiando los contenidos del target contract.
+            filename_temp = create_file(f"transitionss_{idx}", new_dir, self.config_variables.fileName, self.target_contract)
             write_file(filename_temp, body, self.target_contract)
             filename_temp = os.path.realpath(filename_temp)
             contracts.append(filename_temp)
@@ -276,6 +292,44 @@ class VerisolContractCreator(ContractCreator):
 
         return contracts, names_splitted
 
+    def create_transitions_contract_for_thread(self, thread_id, threads_count, results):
+
+        preconditions = np.array_split(self.config_variables.preconditions, threads_count)[thread_id]
+        states = np.array_split(self.config_variables.states, threads_count)[thread_id]
+        extra_conditions = np.array_split(self.config_variables.extraConditions, threads_count)[thread_id]
+
+        if len(preconditions) == 0:
+            # print(f"Preconditions vacío para thread_id={thread_id}, no se crea contrato de transiciones.")
+            results[thread_id] = (None, None)
+            return
+
+        queries, queries_names = self.get_valid_transitions_output(preconditions, self.config_variables.preconditions,states, extra_conditions, self.config_variables.extraConditions,self.config_variables.functions,self.config_variables)
+        if len(queries) == 0:
+            results[thread_id] = (None, None)
+            return
+
+        # Acá metemos las queries que queremos para el nuevo contrato.
+        body = ''.join(queries)
+        body = self.clean_true_requires(body)
+        new_dir = create_directory_2(self.config_variables.dir, f"thread_{thread_id}")
+        # create_file hace un archivo nuevo copiando los contenidos del target contract
+        filename_temp = create_file(f"transitions_{thread_id}", new_dir, self.config_variables.fileName, self.target_contract)
+        write_file(filename_temp, body, self.target_contract)
+        fix_for_version_5(filename_temp)
+        filename_temp = os.path.realpath(filename_temp)
+
+        results[thread_id] = (filename_temp, queries_names)
+
+    def get_valid_preconditions_output(self, preconditions, extra_conditions, function_variables):
+        temp_output = ""
+        temp_function_names = []
+        for id_precondition_require, precondition_require in enumerate(preconditions):
+            function_name = get_temp_function_name(id_precondition_require, "0", "0")
+            temp_function_names.append(function_name)
+            temp_function = functionOutput(function_name, function_variables) + "\n"
+            temp_function += output_valid_state(precondition_require, extra_conditions[id_precondition_require])
+            temp_output += temp_function + "\t}\n"
+        return temp_output, temp_function_names
 
 
 def find_contract_start_line(contract_lines, contract_name):
@@ -305,9 +359,36 @@ def find_contract_end_line(contract_lines, contract_name):
     return -1
 
 
+def is_contract_declaration(line, contract_name):
+    return line.strip().startswith("contract " + contract_name) and not is_a_comment(line)
+
+
+def is_function_declaration(line):
+    return line.strip().startswith("function") and not is_a_comment(line)
+
+
+def is_constructor_declaration(line):
+    return line.strip().startswith("constructor") and not is_a_comment(line)
+
+
 def is_a_comment(line):
     return any(line.strip().startswith(prefix) for prefix in ["//", "/*", "*", "*/"])
 
+def fix_for_version_5(contract_filename):
+    with open(contract_filename, "r") as contract_file:
+        contract = contract_file.read()
+
+    # Replace 'receive()' function declaration with 'function () external payable'
+    contract = re.sub(r'\breceive\(\)\s*(external\s*payable)', r'function () \1', contract)
+
+    # Remove 'abstract' and 'virtual' keywords
+    contract = re.sub(r'\babstract contract\b', 'contract', contract)
+    contract = re.sub(r'\bvirtual\b', '', contract)
+
+    # Replace the 'payable(...)' with the content inside the parentheses
+    contract = re.sub(r'payable\((.*?)\)', r'\1', contract)
+
+    write_file_from_string(contract_filename, contract)
 
 def functionOutput(number, function_variables):
     return "\tfunction vc" + number + "(" + function_variables + ") payable public {"
